543a280ecbf725805868f85c2d8ac149b872250b
midlync <jy27986@gmail.com>
2026-09-09 08:11:10 +0530

    Fix reporter login session handling

--- config/session.php
+++ config/session.php
@@ -28,7 +28,7 @@ return [
 
     'domain' => env('SESSION_DOMAIN', null),
 
-    'secure' => env('SESSION_SECURE_COOKIE', false),
+    'secure' => env('SESSION_SECURE_COOKIE', env('APP_ENV') === 'production'),
 
     'http_only' => env('SESSION_HTTP_ONLY', true),
 
